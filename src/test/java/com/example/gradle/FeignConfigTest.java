986f07253a73d0bcb8a844015d77978ae303dac8
aganchev <alexander.m.ganchev@gmail.com>
2024-06-30 20:38:12 +0300

    Add test for FeignConfig class

---
+++ src/test/java/com/example/gradle/FeignConfigTest.java
@@ -0,0 +1,24 @@
+package com.example.gradle;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class FeignConfigTest {
+    @Autowired
+    private RequestInterceptor requestInterceptor;
+    @Test
+    public void testRequestInterceptor() {
+        RequestTemplate requestTemplate = new RequestTemplate();
+        requestInterceptor.apply(requestTemplate);
+
+        assertEquals("Bearer X0vjEUN6KRlxbp2DoUkyHeM0VOmxY91rA6BbU5j3Xu6wDodwS0McmilLPBWDUcJ1",
+                requestTemplate.headers().get("Authorization").iterator().next());
+    }
+
+}
